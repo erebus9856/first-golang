343795d833079ecbbf397845c7fae803da081247
Daniel Blake <dblake@zai-inc.com>
2018-12-03 18:19:38 -0500

    Made first test work, found my code fails, and then added max/min ranges to int inputs.

--- Iwantto.go
+++ Iwantto.go
@@ -19,8 +19,18 @@ func main() {
   birthData, theName = GetInput()
 
   fmt.Println("\n######################################")
+
+  var ageData [3]float64
+  ageData = CalcAge(birthData)
+
+  fmt.Println(theName, ", you are", ageData[0], "years", ageData[1], "months", ageData[2], "days old")
+
+}
+
+func CalcAge(birthData [3]int) [3]float64 {
   // Get today's date
-  year, month, day := time.Now().Date()
+  year, month, day := nowTime().Date()
+
   // fmt.Println("Year   :", year)
   // fmt.Println("Month  :", int(month))
   // fmt.Println("Day    :", day)
@@ -35,9 +45,12 @@ func main() {
 
   days := math.Ceil(( seconds - ( math.Floor(years) * 86400 * 365.25) ) / 86400)
 
-  fmt.Println(theName, ", you are", years, "years", months, "months", days, "days old")
+  CalcAge := [3]float64 { years, months, days }
+
+  return CalcAge
 
 }
+
 
 // Function to get all reqired data
 func GetInput() ([3]int, string) {
@@ -55,9 +68,9 @@ func GetInput() ([3]int, string) {
   thisText[0] = "Enter the month you were born (1-12): "
   thisText[1] = "Enter the day you were born (1-31): "
   thisText[2] = "Enter the year you were born (YYYY): "
-  birthMonth = GetIntInput(thisText[0])
-  birthDay = GetIntInput(thisText[1])
-  birthYear = GetIntInput(thisText[2])
+  birthMonth = GetIntInput(thisText[0], 1, 12)
+  birthDay = GetIntInput(thisText[1], 1, 31)
+  birthYear = GetIntInput(thisText[2], 0, 9999)
   fmt.Println(birthMonth,"-",birthDay,"-",birthYear)
 
   birthData := [3]int { birthYear, birthMonth, birthDay }
@@ -67,7 +80,7 @@ func GetInput() ([3]int, string) {
 
 // Function to get input for an individual int
 // @// TODO: need to make sure that the int is within the correct range.
-func GetIntInput(displayText string) int {
+func GetIntInput(displayText string, min int, max int) int {
   // Declare the variables
   var validInt bool
   var clean_input string
@@ -83,7 +96,7 @@ func GetIntInput(displayText string) int {
     fmt.Println(displayText)
     input, _ := reader.ReadString('\n')
     clean_input = strings.TrimSuffix(input, "\n")
-    validInt = CheckInput(clean_input)
+    validInt = CheckInput(clean_input, min, max)
   }
 
   myInt, _ = strconv.Atoi(clean_input)
@@ -91,11 +104,12 @@ func GetIntInput(displayText string) int {
 }
 
 // Function to check if an int is input or not
-func CheckInput(clean_input string) bool {
+func CheckInput(clean_input string, min int, max int) bool {
   // Convert string to int
-  _, err := strconv.Atoi(clean_input)
+  thisInt, err := strconv.Atoi(clean_input)
+
   // if it converts correctly, return true
-  if err == nil {
+  if (thisInt >= min && thisInt <= max && err == nil) {
     return true
   } else { // else return false
     return false
@@ -104,4 +118,20 @@ func CheckInput(clean_input string) bool {
 
 func Date(year, month, day int) time.Time {
   return time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.UTC)
+}
+
+
+// Functions to Stub time and set it for testing.
+type TypeNowTime func() time.Time
+
+var nowTime TypeNowTime
+
+func init() {
+    resetClock()
+}
+
+func resetClock() {
+    nowTime = func() time.Time {
+        return time.Now()
+    }
 }
--- bday_test.go
+++ bday_test.go
@@ -1,9 +1,32 @@
 package main
 
-import "testing"
+import (
+  "testing"
+  "time"
+  // "fmt"
+)
 
-func TestSomething(t *testing.T) {
-	// t.Fail()
-  t.Log("First test.")
+func TestDateCalculation(t *testing.T) {
+  nowTime = func() time.Time {
+      return time.Date(1985, 11, 9, 0, 0, 0, 0, time.UTC)
+  }
+  t.Log("Testing to see if this actually calculates the difference between a birthday and a fixed date correctly.")
+  birthData := [3]int { 1982, 12, 2 }
+  var ageData [3]float64
+  ageData = CalcAge(birthData)
 
+  if (ageData[0] != 2) {
+    t.Error("Year Calculation WRONG!!")
+  }
+  if (ageData[1] != 11) {
+    t.Error("Month Calculation WRONG!!!!")
+  }
+  if (ageData[2] != 7) {
+    t.Error("Day Calculation WRONG!!!")
+  }
+
+  resetClock()
 }
+
+// Thanks for the help
+// http://labs.yulrizka.com/en/2014/10/stubbing-time-dot-now-in-golang.html#.XAWrLvxOmEI
